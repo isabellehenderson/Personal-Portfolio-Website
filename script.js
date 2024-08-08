39c8995670afa2d4f160149c64bc2eb24f279b77
Isabelle <isabellehenderson1@gmail.com>
2024-08-08 11:43:05 -0700

    playing with js

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-window.addEventListener('DOMContentLoaded', function() {
-    // Set the scroll restoration mode to 'auto' to prevent the browser from
-    // restoring the scroll position when the page is refreshed
-    history.scrollRestoration = 'auto';
-  
-    // Scroll to the top of the page
-    window.scrollTo(0, 0);
-  });
-  
-  // Smooth scroll for anchor links
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href');
-  
-      if (targetId === '#') {
-        // If the link is the top anchor, scroll to the top immediately
-        window.scrollTo(0, 0);
-      } else {
-        // Otherwise, use smooth scrolling
-        document.querySelector(targetId).scrollIntoView({
-          behavior: 'smooth'
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
-      }
     });
-  });
+});
